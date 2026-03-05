569b65cb7d36fe471dc8c48fef874f4537c01b24
quentinchad <chirat.quentin@gmail.com>
2026-03-05 10:20:13 +0100

    test qualité max 50

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -39,6 +39,13 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(4, items[0].sell_in)
         self.assertEqual(6, items[0].quality)
 
+    def test_qualite_max_50(self):
+        items = [Item("Aged Brie", 5, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)
+
         
 if __name__ == '__main__':
     unittest.main()
