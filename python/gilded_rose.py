3c81ec905a78dbb5e0164283066f76256d126400
quentinchad <chirat.quentin@gmail.com>
2026-03-05 10:27:46 +0100

    refactorisation complete logique avant sell in

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -19,33 +19,20 @@ class GildedRose(object):
             if item.name == "Sulfuras, Hand of Ragnaros":
                 continue
 
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
+            if item.name == "Aged Brie":
+                self.augmenter_qualite(item)
+
+            elif item.name == "Backstage passes to a TAFKAL80ETC concert":
+                self.augmenter_qualite(item)
+
+                if item.sell_in < 11:
+                    self.augmenter_qualite(item)
+
+                if item.sell_in < 6:
+                    self.augmenter_qualite(item)
+
             else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                self.diminuer_qualite(item)
 
 
 class Item:
