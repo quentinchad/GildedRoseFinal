e3e55049ffec318124ab3b3ca9b2c9a3fd25299b
quentinchad <chirat.quentin@gmail.com>
2026-03-05 10:20:43 +0100

    test sulfuras

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -46,6 +46,13 @@ class GildedRoseTest(unittest.TestCase):
         self.assertEqual(4, items[0].sell_in)
         self.assertEqual(50, items[0].quality)
 
+    def test_item_sulfuras_ne_change_pas(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(5, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
         
 if __name__ == '__main__':
     unittest.main()
